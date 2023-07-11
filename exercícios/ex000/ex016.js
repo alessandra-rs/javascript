bdcec83df381131d94773db23f8a71144ca62d71
alessandra-rs <135713931+alessandra-rs@users.noreply.github.com>
2023-07-11 10:07:44 -0300

    ex016

---
+++ exercícios/ex000/ex016.js
@@ -0,0 +1,18 @@
+/* Estrutura de Repetição c/
+   Teste Lógico no Final 
+*/
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ //c + 1
+} while (c <= 6)
+
+
+/* Estrutura de Repetição c/
+   Teste Lógico no Inicio
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ //c + 1
+} 
+*/
